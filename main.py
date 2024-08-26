3e9d36d8555c76a0195babb4468e4321c0b06287
Fleyducc <167763678+Fleyducc@users.noreply.github.com>
2024-08-26 20:31:26 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+print('Mirabbosov Mirsaid Mirjalolovich')
+
+print('11-mart 2009-yil')
+
+print('Olmazor tumani Qora-Qamish 2/5 2/28')
+
+print('O`zbekiston Respublikasi \"1-sentabr\" kuni mustaqilikka erishgan')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print('Hello World')
+# print(123)
+# print(123, 'Hello World')
+
+# son1 = 1
+# son2 = 3
+
+# son3 = 5
+# son4 = 5
+
+# son5 = 15
+# son6 = 3
+
+# son7 = 15
+# son8 = 3
+
+# print(f"{son1} + {son2} = {son1 + son2}")
+
+# print(f"{son3} - {son4} = {son3 - son4}")
+
+# print(f"{son5} * {son6} = {son5 * son6}")
+
+# print(f"{son7} / {son8} = {son7 / son8}")
